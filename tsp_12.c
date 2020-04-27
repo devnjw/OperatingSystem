b2fd675d1019f94c9d442bc24cd7554a7efd8ad0
21600212 <21600212@handong.edu>
2020-04-27 13:47:57 +0900

    9th commit, Rearrange Positions of functions

--- tsp_12.c
+++ tsp_12.c
@@ -12,171 +12,38 @@
 int arr[MAXSIZE][MAXSIZE] ;
 int used[MAXSIZE] ;
 int order[MAXSIZE+1] ;
-int length = 0;
-int N;
-long long count = 0;
-int K;
-int numP = 0 ;
-int cnt = 0 ;
-int curr_cpid[50];
-int parent_pid;
+int best_order[MAXSIZE] ;
 
-int pipe1[2];
-int pipe2[2];
+int length = 0 ;
+int min_length = 1000000 ;
+int N, K ; //N: Num of Nodes, K: Max num of child prcess
+long long count = 0 ; //The number of traveled routes
+int numP = 0 ; //The number of forked child processes
+int cnt = 0 ; //The number of running child processes
+int parent_pid ; //Store parent process pid
 
-int best_order[MAXSIZE];
-int min_length = 1000000;
-
-void push_cpid();
-void del_cpid();
-void print_solution() ;
 void travel(int s, int num) ;
-void print() ;
-pid_t fork_child(int child_num);
-void fileopen(char * fname);
-int factorial(int num);
+void fork_child(int s, int num);
+void distributeTasks(int s, int num) ;
 
 void sigint_handler(int sig) ;
 void sigchld_handler(int sig) ;
+void kill_childs() ;
 
-void parent_read_pipe()
-{
-    long long tmp;
-    int child_length;
-    int tmpArr[MAXSIZE+1];
+int pipe1[2] ;
+void parent_read_pipe() ;
+void child_write_pipe() ;
 
-    close(pipe2[1]) ;
-    read(pipe2[0], &child_length, sizeof(child_length)) ;
-    read(pipe2[0], &tmp, sizeof(tmp)) ;
-    for(int i = 1; i <= N; ++i)
-	read(pipe2[0], &tmpArr[i], sizeof(tmpArr[i])) ;
-    close(pipe2[0]) ;
+void print_solution() ; //Print out current best solution
+void print() ; //Print out Graph
 
-    count += tmp ;
-    if(min_length > child_length){
-	min_length = child_length ;
-	memcpy(best_order+1,tmpArr+1, sizeof(int)*N) ;
-	printf("Updated Status! Min Length: %d\nBest Order: ", min_length) ;
-	for(int i = 1; i <= N; ++i)
-	    printf("%d-",best_order[i]) ;
-	printf("%d\n",best_order[1]) ;	
-    }
-}
+int curr_cpid[50] ;
+void push_cpid() ;
+void del_cpid() ;
 
-void child_write_pipe()
-{
-    close(pipe2[0]) ;
-    write(pipe2[1], &min_length, sizeof(min_length)) ;
-    write(pipe2[1], &count, sizeof(count)) ;
-    for(int i = 1; i <= N; ++i)
-	write(pipe2[1], &best_order[i], sizeof(best_order[i])) ;
-    close(pipe2[1]) ;
-}
+void fileopen(char * fname) ;
+int factorial(int num) ;
 
-void forkChild(int s, int num){    
-    pid_t child_pid;
-    
-    if(pipe(pipe1) != 0){
-	perror("Error") ;
-	exit(1) ;
-    }
-    pipe(pipe2) ;
-
-    if(cnt == K)
-	wait(0);
-
-    if(cnt < K){
-	cnt++;
-	numP++;
-	child_pid = fork();
-    }
-
-    if(child_pid > 0){
-	printf("Child %d is forked\n", child_pid);
-	//sleep(1);
-	push_cpid(child_pid);
-	parent_read_pipe() ;
-    }
-    else if(child_pid == 0){
-	count = 0;
-	//int parent_len = min_length;
-	travel(s, num);
-	
-	//if(min_length > parent_len)
-	child_write_pipe();
-	//sleep(10);
-/*
-    	for(int i = 1; i <= N; ++i)
-	    printf("%d-", best_order[i]);
-	printf("%d length: %d\n", best_order[1], min_length);
-	*/
-	exit(0) ;
-    }
-}
-
-void distributeTasks(int s, int num){
-    used[s] = 1;
-    order[num] = s;
-
-    // If condition satisfied, the remaining workload is 12!
-    // Time to distribute the task to the Child Process.
-    if(num == N - MAXTASK){
-	forkChild(s, num);
-    }
-    else{
-	for(int i = 0; i < N; ++i){
-	    if(used[i]!=1){
-		length += arr[s][i];
-		distributeTasks(i, num+1);
-		length -= arr[s][i];
-	    }
-	}
-    }   
-    
-    used[s] = 0 ;
-    order[num] = -1;
-}
-
-void sigchld_handler(int sig)
-{
-    int exitcode ;
-    pid_t child = wait(0x0) ;
-    printf("> child process %d is terminated\n", child) ;
-
-    del_cpid(child) ;
-    cnt--;
-
-    //close(pipes[0]) ;
-    //write(pipes[1], &min_length, sizeof(min_length)) ;
-    //close(pipes[1]) ;
-}
-
-void kill_childs()
-{
-    parent_read_pipe() ;
-    for(int i = 0; i < K; ++i){
-	printf("Kill process %d\n", curr_cpid[i]);
-	if(curr_cpid[i]>0)
-	    kill(curr_cpid[i], SIGINT) ;
-	wait(0x0);
-    }
-}
-
-void sigint_handler(int sig)
-{
-    //printf("Process %d entered here\n", getpid()) ;
-    if(getpid() == parent_pid){
-	kill_childs() ;	
-	print_solution() ;
-    }
-    else{
-	printf("I'm a child %d, I want to write pipe\n", getpid()) ;
-	print_solution() ;
-	child_write_pipe() ;
-    }
-
-    exit(0);
-}
 
 int main(int argc, char* argv[]) {
     K = atoi(argv[2]);
@@ -202,21 +69,6 @@ int main(int argc, char* argv[]) {
     double time_spent = (double)(end - begin) / CLOCKS_PER_SEC ;
     printf("Execution time : %f\n", time_spent);
 }
-void print_solution(){
-    //Print out Solution
-    printf("The best solution route: [");
-    for(int i = 1; i <= N; ++i)
-	printf("%d-", best_order[i]);
-    printf("%d], length: %d\n", best_order[1], min_length);
-    printf("The total number of traveled routes: %lld\n", count);
-}
-
-pid_t fork_child(int child_num){
-    pid_t child_pid ;
-    child_pid = fork();
-    
-    return child_pid;
-   }
 
 void travel(int s, int num){
     used[s] = 1 ;
@@ -248,6 +100,153 @@ void travel(int s, int num){
 
     used[s] = 0 ;
     order[num] = -1;
+}
+
+void forkChild(int s, int num){    
+    pid_t child_pid;
+    
+    if(pipe(pipe1) != 0){
+	perror("Error") ;
+	exit(1) ;
+    }
+
+    if(cnt == K)
+	wait(0);
+    if(cnt < K){
+	cnt++;
+	numP++;
+	child_pid = fork();
+    }
+
+    if(child_pid > 0){
+	//printf("Child %d is forked\n", child_pid);
+
+	push_cpid(child_pid);
+	parent_read_pipe() ;
+    }
+    else if(child_pid == 0){
+	count = 0;
+	travel(s, num);
+
+	child_write_pipe();
+
+	exit(0) ;
+    }
+}
+
+void distributeTasks(int s, int num){
+    used[s] = 1;
+    order[num] = s;
+
+    // If condition satisfied, the remaining workload is 12!
+    // Time to distribute the task to the Child Process.
+    if(num == N - MAXTASK){
+	forkChild(s, num);
+    }
+    else{
+	for(int i = 0; i < N; ++i){
+	    if(used[i]!=1){
+		length += arr[s][i];
+		distributeTasks(i, num+1);
+		length -= arr[s][i];
+	    }
+	}
+    }   
+    
+    used[s] = 0 ;
+    order[num] = -1;
+}
+
+//---------------< Signal Handlers >---------------//
+void sigchld_handler(int sig)
+{
+    int exitcode ;
+    pid_t child = wait(0x0) ;
+    printf("> child process %d is terminated\n", child) ;
+
+    del_cpid(child) ;
+    cnt--;
+}
+
+void sigint_handler(int sig)
+{
+    if(getpid() == parent_pid){
+	kill_childs() ;	
+	print_solution() ;
+    }
+    else{
+	printf("I'm a child %d, I want to write pipe\n", getpid()) ;
+	print_solution() ;
+	child_write_pipe() ;
+    }
+    exit(0);
+}
+
+void kill_childs()
+{
+    parent_read_pipe() ;
+    for(int i = 0; i < K; ++i){
+	printf("Kill process %d\n", curr_cpid[i]);
+	if(curr_cpid[i]>0)
+	    kill(curr_cpid[i], SIGINT) ;
+	wait(0x0);
+    }
+}
+
+//---------------< Pipe Operators >---------------//
+void parent_read_pipe()
+{
+    long long tmp;
+    int child_length;
+    int tmpArr[MAXSIZE+1];
+
+    close(pipe1[1]) ;
+    read(pipe1[0], &child_length, sizeof(child_length)) ;
+    read(pipe1[0], &tmp, sizeof(tmp)) ;
+    for(int i = 1; i <= N; ++i)
+	read(pipe1[0], &tmpArr[i], sizeof(tmpArr[i])) ;
+    close(pipe1[0]) ;
+
+    count += tmp ;
+    if(min_length > child_length){
+	min_length = child_length ;
+	memcpy(best_order+1,tmpArr+1, sizeof(int)*N) ;
+	//printf("Updated Status! Min Length: %d\nBest Order: ", min_length) ;
+	//for(int i = 1; i <= N; ++i)
+	    //printf("%d-",best_order[i]) ;
+	//printf("%d\n",best_order[1]) ;	
+	print_solution() ;
+    }
+}
+
+void child_write_pipe()
+{
+    close(pipe1[0]) ;
+    write(pipe1[1], &min_length, sizeof(min_length)) ;
+    write(pipe1[1], &count, sizeof(count)) ;
+    for(int i = 1; i <= N; ++i)
+	write(pipe1[1], &best_order[i], sizeof(best_order[i])) ;
+    close(pipe1[1]) ;
+}
+
+
+//---------------< Utilites >---------------//
+void print_solution(){
+    //Print out Solution
+    printf("The best solution route: [");
+    for(int i = 1; i <= N; ++i)
+	printf("%d-", best_order[i]);
+    printf("%d], length: %d\n", best_order[1], min_length);
+    printf("The total number of traveled routes: %lld\n", count);
+}
+
+void print(){
+    for(int i = 0; i < N; ++i){
+	for(int j = 0; j < N; ++j){
+	    printf("%d ", arr[i][j]);
+	}
+    printf("\n");
+    }
 }
 
 void push_cpid(int cpid)
@@ -292,11 +291,3 @@ int factorial(int num)
     return num*factorial(num-1);
 }
 
-void print(){
-    for(int i = 0; i < N; ++i){
-	for(int j = 0; j < N; ++j){
-	    printf("%d ", arr[i][j]);
-	}
-    printf("\n");
-    }
-}
