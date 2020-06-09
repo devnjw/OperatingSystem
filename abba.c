cf579077ea886ce462885f915bff5946165fbf08
21600212 <21600212@handong.edu>
2020-06-09 17:58:40 +0900

    Almost done

--- abba.c
+++ abba.c
@@ -1,8 +1,11 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <time.h>
 #include <unistd.h>
+#include <dlfcn.h>
+#include <execinfo.h>
 
 pthread_mutex_t mutex0 = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
@@ -15,7 +18,9 @@ pthread_mutex_t mutex6 = PTHREAD_MUTEX_INITIALIZER;
 void 
 noise()
 {
-	usleep(rand() % 1000) ;
+    usleep(rand() % 1000) ;
+    //sleep(3);
+
 }
 
 void *
@@ -24,8 +29,8 @@ thread1(void *arg)
 		pthread_mutex_lock(&mutex1); noise() ;
 		pthread_mutex_lock(&mutex2); noise() ;
 
-		//pthread_mutex_lock(&mutex4); noise() ;
-		//pthread_mutex_lock(&mutex3); noise() ;
+		pthread_mutex_unlock(&mutex1); noise() ;
+		pthread_mutex_unlock(&mutex2); noise() ;
 
 		return NULL;
 }
@@ -34,25 +39,62 @@ void *
 thread2(void *arg)
 {
     pthread_mutex_lock(&mutex2); noise() ;
-    pthread_mutex_lock(&mutex0); noise() ;
+    pthread_mutex_lock(&mutex3); noise() ;
+
+    pthread_mutex_unlock(&mutex2); noise() ;
+    pthread_mutex_unlock(&mutex3); noise() ;
+}
+
+void *
+thread3(void *arg)
+{
+    pthread_mutex_lock(&mutex3); noise() ;
+    pthread_mutex_lock(&mutex4); noise() ;
+
+    pthread_mutex_unlock(&mutex3); noise() ;
+    pthread_mutex_unlock(&mutex4); noise() ;
+}
+
+void *
+thread4(void *arg)
+{
+    pthread_mutex_lock(&mutex4); noise() ;
+    pthread_mutex_lock(&mutex5); noise() ;
+
+    pthread_mutex_unlock(&mutex4); noise() ;
+    pthread_mutex_unlock(&mutex5); noise() ;
+}
+
+void *
+thread5(void *arg)
+{
+    pthread_mutex_lock(&mutex5); noise() ;
+    //pthread_mutex_lock(&mutex2);
+    noise() ;
+
+    pthread_mutex_unlock(&mutex5); noise() ;
+    //pthread_mutex_unlock(&mutex2); noise() ;
 }
 
 int 
 main(int argc, char *argv[]) 
 {
-	pthread_t tid1, tid2;
+	pthread_t tid1, tid2, tid3, tid4, tid5;
 	srand(time(0x0)) ;
 
-	pthread_create(&tid1, NULL, thread1, NULL);
-	pthread_create(&tid2, NULL, thread2, NULL);
-		
-	pthread_mutex_lock(&mutex0); noise() ; 
-	pthread_mutex_lock(&mutex1); noise() ; 
-
+	pthread_create(&tid1, NULL, thread5, NULL);
+	pthread_create(&tid2, NULL, thread4, NULL);
+	pthread_create(&tid3, NULL, thread3, NULL);
+	pthread_create(&tid4, NULL, thread2, NULL);
+	pthread_create(&tid5, NULL, thread1, NULL);
 
 
 	pthread_join(tid1, NULL);
 	pthread_join(tid2, NULL);
+	pthread_join(tid3, NULL);
+	pthread_join(tid4, NULL);
+	pthread_join(tid5, NULL);
+	
 
 	return 0;
 }
--- ddmon.c
+++ ddmon.c
@@ -11,15 +11,8 @@
 #include <execinfo.h>
 #include <pthread.h>
 
-int
-pthread_mutex_lock(pthread_mutex_t *lock)
+int pthread_mutex_lock(pthread_mutex_t *lock)
 {
-    if(mkfifo(".ddrace", 0666)){
-	if(errno != EEXIST) {
-	    perror("fail to open fifo: ") ;
-	    exit(1) ;
-	}
-    }
     int fd = open(".ddtrace", O_WRONLY | O_SYNC) ;
 
     void (*lockp)(void *) = NULL ;
@@ -30,7 +23,6 @@ pthread_mutex_lock(pthread_mutex_t *lock)
 	exit(1) ;
     
     long long pass = (long long)(lock);
-    printf("mutex sting : %lld\n", pass);
 
     char buf[128];
     char temp[128];
@@ -41,14 +33,12 @@ pthread_mutex_lock(pthread_mutex_t *lock)
     sprintf(temp, "%ud", (int)tid);
     strcat(buf, " ");
     strcat(buf, temp);
+    strcat(buf, " 1"); // put '1' to let ddchck know this is 'lock' function.
 
     write(fd, buf, 128) ;
-
-    printf("Thread id: %ud\n", (int)tid) ;
     close(fd) ;
 
     lockp(lock) ;
-    printf("Mutex lock out\n") ;
 
     return 0;
 }
@@ -56,7 +46,8 @@ pthread_mutex_lock(pthread_mutex_t *lock)
 int
 pthread_mutex_unlock(pthread_mutex_t *lock)
 {
-    printf("Mutex unlock in\n");
+    int fd = open(".ddtrace", O_WRONLY | O_SYNC) ;
+
     void (*unlockp)(void *) = NULL ;
     char * error ;
 
@@ -64,8 +55,23 @@ pthread_mutex_unlock(pthread_mutex_t *lock)
     if ((error = dlerror()) != 0x0)
 	exit(1) ;
 
+    long long pass = (long long)(lock);
+
+    char buf[128];
+    char temp[128];
+
+    sprintf(buf, "%lld", pass) ;
+
+    pthread_t tid = pthread_self();
+    sprintf(temp, "%ud", (int)tid);
+    strcat(buf, " ");
+    strcat(buf, temp);
+    strcat(buf, " 2"); // put '2' to let ddchck know this is 'unlock' function.
+
+    write(fd, buf, 128) ;
+    close(fd) ;
+
     unlockp(lock) ;
-    printf("Mutex unlock out\n") ;
 
     return 0;
 }
